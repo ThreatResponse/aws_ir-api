3119ce15aa3c1139b3ffc1b8ace0bace6d833384
andrewkrug@gmail.com <andrewkrug@gmail.com>
2016-12-10 12:29:14 -0800

    all plugins loading through gross chalice path requirements

--- aws_ir-api/app.py
+++ aws_ir-api/app.py
@@ -1,11 +1,26 @@
 from chalice import Chalice
 
+#The aws_ir modules we need to drive the API
+
+#key compromise plugins
+from chalicelib.aws_ir.aws_ir.plugins import gather_host
+#from aws_ir.plugins import revokests_key
+
+#host compromise plugins
+from chalicelib.aws_ir.aws_ir.plugins import gather_host
+from chalicelib.aws_ir.aws_ir.plugins import isolate_host
+from chalicelib.aws_ir.aws_ir.plugins import snapshotdisks_host
+from chalicelib.aws_ir.aws_ir.plugins import stop_host
+from chalicelib.aws_ir.aws_ir.plugins import tag_host
+
+#to-do find a way to async gather memory
+
 app = Chalice(app_name='aws_ir-api')
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return {'AWS_IR-api experimental'}
 
 
 # The view function above will return {"hello": "world"}
