36c539a9c53b888b799b2a406211d84bb963ea5b
andrewkrug <andrewkrug@gmail.com>
2016-12-30 09:59:22 -0800

    fix credential check

--- api/app.py
+++ api/app.py
@@ -28,41 +28,21 @@ def index():
     #credential.Credential('fhsdfdsfkjhfs')
     return {'AWS_IR-api': 'experimental'}
 
-@app.route('/credential', methods=['POST'], api_key_required=False)
+@app.route('/credential', methods=['POST'], api_key_required=True)
 def credential_get():
-    post = app.current_request.json_body
-
-    c = credential.Credential(post['sort_key'])
-    print c.table_name
-
     try:
-        check = c.check('read')
+        post = app.current_request.json_body
+        c = credential.Credential(post['sort_key'])
+        if post['operation']:
+            check = c.check(post['operation'])
         if check == True:
             return {'status': 'valid'}
         else:
             return {'status': 'invalid'}
     except:
         print("Exception occured while calling")
-        return {'status': 'invalid'}
+        return {'status': 'malformed-payload'}
 
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
+@app.route('/key/{plugin}', methods=['POST, GET'], api_key_required=True)
+def credential_get(plugin):
+    pass
