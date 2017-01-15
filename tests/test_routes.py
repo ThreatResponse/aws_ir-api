20b042a79cb4af6212b5329556c91490b2d5e0a6
andrewkrug <andrewkrug@gmail.com>
2017-01-15 09:35:48 -0800

    add test_route stub

---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import pytest
+import boto3
+import base64
+import os
+import json
+import time
+import uuid
+
+from faker import Factory
+from dotenv import Dotenv
+from api import app as awsirapi
+
+
+
+def test_index():
+    result = awsirapi.index()
+    assert result == {'AWS_IR-api': 'experimental'}
