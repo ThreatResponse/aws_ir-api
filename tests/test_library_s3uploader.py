5792cf57b969c59425c9e702e5df20fcfeb06e9c
andrewkrug <andrewkrug@gmail.com>
2017-02-02 09:38:10 -0800

    tests fail

--- tests/test_library_s3uploader.py
+++ tests/test_library_s3uploader.py
@@ -1,19 +1,52 @@
 import pytest
 import boto3
 import hashlib
+import uuid
 
 from api.chalicelib import uploader
 
 CLIENT = boto3.client('s3')
+UUID = uuid.uuid4().hex
 
 def setup_test():
+    response = CLIENT.create_bucket(
+        Bucket=UUID,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'us-west-2'
+        }
+    )
     pass
+
+def test_initialize():
+    u = uploader.S3Uploader(
+        CLIENT,
+        UUID
+    )
+
+    assert u is not None
 
 def test_upload():
-    pass
+    u = uploader.S3Uploader(
+        CLIENT,
+        UUID
+    )
+
+    result = u.upload('Thisisasampleofsomecontent')
+    assert result is not None
+
 
 def test_validate():
-    pass
+    u = uploader.S3Uploader(
+        CLIENT,
+        UUID
+    )
+
+    result = u.validate('Thisisasampleofsomecontent')
+    assert result is True
+
 
 def teardown_test():
+    response = CLIENT.delete_bucket(
+        Bucket=UUID
+    )
     pass
